ad1c3df5b89fc88d5393b3674e894f32f7bde198
JF <jfan9988@gmail.com>
2020-05-24 14:47:22 +1000

    implement web workers

--- docs/handpose.js
+++ docs/handpose.js
@@ -1,17 +1,35 @@
-import * as handpose from "@tensorflow-models/handpose";
+import * as Comlink from "comlink";
+
+const HandDetector = Comlink.wrap(new Worker("./handpose-worker.js"));
+let handDetector;
+const videoElement = document.getElementById("webcam-video");
+const canvasElement = document.getElementById("webcam-canvas");
 
 const initHandposeDetection = async (hands) => {
-  let model = await handpose.load();
+  handDetector = await new HandDetector();
   console.log("handpose loaded");
 
   const estimateHandPose = async () => {
-    const videoElement = document.getElementById("webcam-video");
+    const context = canvasElement.getContext("2d");
+    context.drawImage(
+      videoElement,
+      0,
+      0,
+      videoElement.width,
+      videoElement.height
+    );
+    var data = context.getImageData(
+      0,
+      0,
+      videoElement.width,
+      videoElement.height
+    );
 
-    const handEstimate = await model.estimateHands(videoElement, true);
-    if (!handEstimate) {
-      return;
+    const estimation = await handDetector.estimateHandPose(data);
+    if (estimation) {
+      hands.data = estimation;
     }
-    hands.data = handEstimate;
+    console.log("detecting hands", estimation, hands.data);
   };
 
   const runDetection = async () => {
