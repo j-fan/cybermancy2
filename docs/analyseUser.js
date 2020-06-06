153d31e98c52e2d78a67d5a56ac86d0aafc608e0
jfan <jfan9988@gmail.com>
2020-06-06 21:29:30 +1000

    implement hand element detection

--- docs/analyseUser.js
+++ docs/analyseUser.js
@@ -1,13 +1,66 @@
 import { estimatedAge, estimatedGender } from "./faceDetect";
-import { hands, isHandPresent, newHandAppeared } from "./handpose";
+import { hands, newHandAppeared } from "./handpose";
+import * as elementContent from "./elements.json";
 
-const getHandElement = () => {};
+let ageGenderContent = [];
+let handElement = "";
+
+const distance = (x1, y1, z1, x2, y2, z2) => {
+  const x = x1 - x2;
+  const y = y1 - y2;
+  const z = z1 - z2;
+  return Math.sqrt(x * x + y * y + z * z);
+};
+
+const distanceWithArray = (p1, p2) =>
+  distance(p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]);
+
+const getHandElement = () => {
+  if (newHandAppeared) {
+    const annotations = hands[0].annotations;
+    const palmWidth = distanceWithArray(
+      annotations.palmBase[0],
+      annotations.middleFinger[0]
+    );
+    const fingerLength = distanceWithArray(
+      annotations.middleFinger[0],
+      annotations.middleFinger[3]
+    );
+    const palmLength = distanceWithArray(
+      annotations.indexFinger[0],
+      annotations.pinky[0]
+    );
+
+    const palmRatio = palmLength / palmWidth;
+    const fingerToPalmRatio = fingerLength / palmLength;
+
+    const isLongFingers = fingerToPalmRatio > 1.1;
+    const isWideHand = palmRatio > 1;
+
+    if (!isWideHand && isLongFingers) {
+      handElement = "Water";
+    } else if (!isWideHand && !isLongFingers) {
+      handElement = "Fire";
+    } else if (isWideHand && isLongFingers) {
+      handElement == "Air";
+    } else {
+      handElement == "Earth";
+    }
+    console.log(handElement);
+  }
+
+  return handElement ? elementContent[handElement] : "";
+};
 
 const getAgeGenderContent = () => {
+  getHandElement();
   if (newHandAppeared) {
-    console.log("getGenderAge", estimatedGender, estimatedAge, newHandAppeared);
-    console.log("-----");
+    const key = `${Math.floor(estimatedAge / 10) * 10}-${
+      (Math.floor(estimatedAge / 10) + 1) * 10
+    }`;
+    console.log("getGenderAge", key, newHandAppeared);
   }
+  return ageGenderContent;
 };
 
 export { getHandElement, getAgeGenderContent };
