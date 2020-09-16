a31117b76b2e2fa322380c0815b7bbac6c9ed09b
JF <jfan9988@gmail.com>
2020-09-16 13:02:39 +1000

    reload periodically

--- src/loadingScreen.js
+++ src/loadingScreen.js
@@ -2,12 +2,11 @@ const loadingScreen = document.getElementById("loadingScreen");
 const loadingButton = document.getElementById("loadingButton");
 
 const hideLoadingScreen = () => {
-  // loadingButton.addEventListener("click", () => {
-  //   loadingScreen.classList.add("hidden");
-  // })
-  // loadingButton.classList.remove("hidden");
-  // loadingButton.innerHTML = "Enter artwork";
   loadingScreen.classList.add("hidden");
+
+  setInterval(function () {
+    location.reload();
+  }, 1800000);
 };
 
 export { hideLoadingScreen };
